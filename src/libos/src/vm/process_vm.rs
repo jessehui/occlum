24af763dc8b1fd6f3cbc4078a27382a0b7a97acd
Hui, Chunyang <sanqian.hcy@antfin.com>
2023-07-14 08:06:32 +0000

    Spark can pass

--- src/libos/src/vm/process_vm.rs
+++ src/libos/src/vm/process_vm.rs
@@ -547,7 +547,7 @@ impl ProcessVM {
                 PagePolicy::CommitNow
             // } else if perms != VMPerms::DEFAULT && perms != VMPerms::NONE {
             //     PagePolicy::CommitNow
-            } else if perms == VMPerms::ALL {
+            } else if perms != VMPerms::DEFAULT {
                 PagePolicy::CommitNow
             } else {
                 PagePolicy::CommitOnDemand
